e89de731c197280d06a162b4686f31e06464032c
Tony Riverol <riverol26@aol.com>
2017-11-01 22:48:48 -0400

    Add PATCH /todos/:todoID

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -64,7 +65,28 @@ app.delete('/todos/:todoID', function(req, res){
     })
 });
 
-
+app.patch('/todos/:todoID', function(req, res){
+    var id = req.params.todoID;
+    var body = _.pick(req.body, ['text', 'completed']);
+    var d = new Date();
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = d.getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(function(todo){
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(function(err){
+        res.status(400).send();
+    })
+});
 
 
 
