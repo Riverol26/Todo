91c8eac1ec0e7e8d26e57c028fe9c22aa4ab0076
Tony Riverol <riverol26@aol.com>
2017-11-07 12:36:22 -0500

    Hash user passwords before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,19 +1,45 @@
 const {SHA256} = require('crypto-js');
-
 const jwt = require('jsonwebtoken');
-
-var data = {
-    id: 10
-};
-
-var token = jwt.sign(data, '123abc');
-console.log(token);
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+const bcrypt = require('bcryptjs');
 
 
 
+var password = '123abc!';
+
+// bcrypt.genSalt(10, function(err, salt){
+//     bcrypt.hash(password, salt, function(err, hash){
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$gjd4dBHyJvykBfeA.9R6/uRvS.SdWe05k3lcCFUIMeo4k3f2eHkgi';
+
+bcrypt.compare(password, hashedPassword, function(err, res){
+    console.log(res);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
+
+//--------------------------------------------------
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
@@ -32,6 +58,7 @@ console.log('decoded: ', decoded);
 // token.data.id = 5;
 // token.hash = SHA256(JSON.stringify(token.data)).toString();
 
+//-------------------------------------------------------------------
 
 // var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
 
